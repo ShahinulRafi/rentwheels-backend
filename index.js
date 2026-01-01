66b7d2b07f5672396e30d06d88eacb2387e8544c
ShahinulRafi <shahinul.csecu@gmail.com>
2026-01-01 07:06:47 +0600

    my bookings backend

--- index.js
+++ index.js
@@ -31,6 +31,27 @@ async function run() {
         const result = await carServices.insertOne(car);
         res.send(result);
     })
+
+    //get services from data
+    app.get('/services', async(req, res) => {
+      const result = await carServices.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/services/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await carServices.findOne(query);
+        res.send(result);
+    })
+
+    app.get('/myBookings', async (req, res) => {
+      const {email} = req.query;
+      const query = {email: email}
+      const result = await carServices.find(query).toArray();
+      res.send(result);
+    })
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
